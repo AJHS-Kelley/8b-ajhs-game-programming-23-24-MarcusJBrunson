42c51f0639873d14f6bf436547a56aee265d2058
MarcusJBrunson <s11758661@students.duvalschools.org>
2023-10-17 14:28:05 -0400

    Update collections0.py
    creating own list and sorting that list

--- gameProgramming/computer_science_exercises/02_Collections/collections0.py
+++ gameProgramming/computer_science_exercises/02_Collections/collections0.py
@@ -1,15 +1,30 @@
-playerInventory = [] 
+# playerInventory = [] 
 
-while len(playerInventory) < 10: 
-    item = input("What item do you want to add to the inventory?\n") 
-    playerInventory.append(item)
+# while len(playerInventory) < 10: 
+#     item = input("What item do you want to add to the inventory?\n") 
+#     playerInventory.append(item)
 
-playerInventory.sort()
-print(playerInventory) 
+# playerInventory.sort()
+# print(playerInventory) 
 
-while len(playerInventory) > 5: 
-    item = input("What item do you want to remove to the inventory?\n") 
-    playerInventory.remove(item) 
+# while len(playerInventory) > 5: 
+#     item = input("What item do you want to remove to the inventory?\n") 
+#     playerInventory.remove(item) 
 
-playerInventory.sort()
-print(playerInventory) 
+# playerInventory.sort()
+# print(playerInventory) 
+
+# doorKeys = [
+# "red", 
+# "green", 
+# "yellow", 
+# "purple", 
+# "brown"
+# ]
+
+# key = input("Which color key do you need to unlock the door?\n") 
+
+# if key in doorKeys: 
+#     print("You have the correct key! The door unlocks.\n") 
+# else: 
+#     print("You do not have that key. The door remains locked.\n") 
