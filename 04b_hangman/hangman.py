83e4f5447569746b15ca9b872699e3551972bc9e
MarcusJBrunson <s11758661@students.duvalschools.org>
2023-10-31 14:18:46 -0400

    Update hangman.py

--- 04b_hangman/hangman.py
+++ 04b_hangman/hangman.py
@@ -2,3 +2,50 @@
 
 words = 'Horse Cat Lion Cow Goat Sheep Bear Cheetah Gorilla Camel aggressive futuristic diverse fraudulent dictionary mitochondrion chromosome endomembrane vacuole'.split()
 
+# VARIABLE_NAMES IN ALL-CAPS ARE CONSTANTS ARE NOT MEANT TO CHANGE!
+HANGMAN_BOARD =['''
+    +---+
+        |
+        |
+        |
+    ======''','''
+    +---+
+    O       |
+        |
+        |
+    ======''','''
+    +---+
+        |
+        |
+        |
+    ======''','''
+    +---+
+    O       |
+   /|       |
+        |
+    ======''','''
+    +---+
+        |
+        |
+        |
+    ======''','''
+    +---+
+    O       |
+   /|\   |
+        |
+    ======''','''
+    +---+
+        |
+        |
+        |
+    ======''','''
+    +---+
+    O       |
+   /|\    |
+  /    |
+    ======''',] 
+
+i = 0 
+while i < len(HANGMAN_BOARD):
+    print(HANGMAN_BOARD[i])
+    i += 1
