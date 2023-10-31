bfc3e79ea67841482255595f90a13d8b47dcdf8e
MarcusJBrunson <s11758661@students.duvalschools.org>
2023-10-31 13:59:37 -0400

    Update hangman.py

--- 04b_hangman/hangman.py
+++ 04b_hangman/hangman.py
@@ -1,1 +1,4 @@
-# Hangman Game by Marcus Brunson, v0.0
+# Hangman Game by Marcus Brunson, v0.1
+
+words = 'Horse Cat Lion Cow Goat Sheep Bear Cheetah Gorilla Camel aggressive futuristic diverse fraudulent dictionary mitochondrion chromosome endomembrane vacuole'.split()
+
