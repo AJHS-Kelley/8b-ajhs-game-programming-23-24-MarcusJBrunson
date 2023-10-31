4afaad624c520ae9a8189dada3c33cf7df2b6b0b
MarcusJBrunson <s11758661@students.duvalschools.org>
2023-10-31 14:50:13 -0400

    Update hangman.py

--- 04b_hangman/hangman.py
+++ 04b_hangman/hangman.py
@@ -1,5 +1,5 @@
-# Hangman Game by Marcus Brunson, v0.1
-
+# Hangman Game by Marcus Brunson, v0.3
+import random 
 words = 'Horse Cat Lion Cow Goat Sheep Bear Cheetah Gorilla Camel aggressive futuristic diverse fraudulent dictionary mitochondrion chromosome endomembrane vacuole'.split()
 
 # VARIABLE_NAMES IN ALL-CAPS ARE CONSTANTS ARE NOT MEANT TO CHANGE!
@@ -10,29 +10,29 @@ HANGMAN_BOARD =['''
         |
     ======''','''
     +---+
-    O       |
+    O   |
         |
         |
     ======''','''
     +---+
-        |
-        |
+    O    |
+    |   |
         |
     ======''','''
     +---+
     O       |
    /|       |
-        |
+            |
     ======''','''
     +---+
-        |
-        |
-        |
-    ======''','''
-    +---+
-    O       |
+    O    |
    /|\   |
         |
+    ======''','''
+    +---+
+    O    |
+   /|\   |
+   /      |
     ======''','''
     +---+
         |
@@ -42,10 +42,27 @@ HANGMAN_BOARD =['''
     +---+
     O       |
    /|\    |
-  /    |
+   / \ |
     ======''',] 
 
-i = 0 
-while i < len(HANGMAN_BOARD):
-    print(HANGMAN_BOARD[i])
-    i += 1
+# Pick Word from List 
+def getRandomWord(wordList): # Return a random word from the list
+    wordIndex = random.randint(0, len(wordList)-1) 
+    # len(listname) -1 is extremely common for working with lists.
+    return wordList[wordIndex] 
+
+def displayBoard(missedLetters, correctLetters, sercetWord):
+    print(HANGMAN_BOARD[len(missedLetters)])
+    print()
+
+    print('Missed Letters:', end = ' ')
+    for eachLetter in missedLetters:
+        print(eachLetter, end = ' ')
+    print()
+
+# i = 0
+# while i < 50:
+#     word = getRandomWord(words)
+#     print(word)
+#     i += 1
+
