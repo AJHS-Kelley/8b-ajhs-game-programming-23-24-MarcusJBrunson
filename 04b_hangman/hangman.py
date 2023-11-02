cfd2ef4404dade595e8a1da72cbd667536a68745
MarcusJBrunson <s11758661@students.duvalschools.org>
2023-11-02 14:47:23 -0400

    Update hangman.py

--- 04b_hangman/hangman.py
+++ 04b_hangman/hangman.py
@@ -106,7 +106,35 @@ while True:
     if guess in secretWord:
         correctLetters = correctLetters + guess 
 
-        
+        # Check to See if Winner, Winner Chicken Dinner
+        foundAllLetters = True
+        for i in range(len(secretWord)):
+            if secretWord[i] not in correctLetters:
+                foundAllLetters = False 
+                break
+            if foundAllLetters:
+                print('Much wow! Very win! Well done.')
+                print('The secret word was' + secretWord)
+                gameIsDone = True
+    else:
+        missedLetters = missedLetters + guess
+
+        if len(missedLetters) == len(HANGMAN_BOARD) - 1:
+            displayBoard(missedLetters, correctLetters, secretWord) 
+            print('You have run out of guesses and lost the game.')
+            print('You made this number of correct guesses' + str(len(correctLetters)))
+            print('The secret word was' + secretWord)
+            gameIsDone = True
+    
+    if gameIsDone:
+        if playAgain():
+            missedLetters = ''
+            correctLetters = ''
+            gameIsDone = False
+            secretWord = getRandomWord(words)
+        else:
+            break
+
 
 # i = 0
 # while i < 50:
