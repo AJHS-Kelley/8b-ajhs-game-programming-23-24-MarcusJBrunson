85a25d078350934bb6228603ef00c8a82b89764e
MarcusJBrunson <s11758661@students.duvalschools.org>
2023-11-06 14:27:00 -0500

    Update hangman.py

--- 04b_hangman/hangman.py
+++ 04b_hangman/hangman.py
@@ -1,6 +1,14 @@
 # Hangman Game by Marcus Brunson, v0.3
 import random 
-words = 'Horse Cat Lion Cow Goat Sheep Bear Cheetah Gorilla Camel aggressive futuristic diverse fraudulent dictionary mitochondrion chromosome endomembrane vacuole'.split()
+#words = 'Horse Cat Lion Cow Goat Sheep Bear Cheetah Gorilla Camel aggressive futuristic diverse fraudulent dictionary mitochondrion chromosome endomembrane vacuole'.split()
+# DICTIONARY VERSION
+# Stored in Key:Value Pairs.
+# Actual Dictionary Word (Key) : Value (Definition)
+# Uses {} to specifiy a dictionary.
+words = {'Colors': 'red orange yellow green blue black white sliver gold teal'.split(),
+         'Animals: 'cat cow dog moose duck fish wombat wolf lion alligator giraffe'.split(), 
+         'Shapes': 'square triangle circle rhombus parallelogram trapezoid diamond hexagon'.split(), 
+         'Foods': 'hamburger hotdog potato waffle pancake chips steak oysters chicken'.split()}
 
 # VARIABLE_NAMES IN ALL-CAPS ARE CONSTANTS ARE NOT MEANT TO CHANGE!
 HANGMAN_BOARD =['''
@@ -32,17 +40,21 @@ HANGMAN_BOARD =['''
     +---+
     O    |
    /|\   |
-   /      |
-    ======''','''
+   /     |
+    ======''','''  
     +---+
-        |
-        |
-        |
-    ======''','''
-    +---+
-    O       |
-   /|\    |
-   / \ |
+    O   |
+   /|\  |
+   / \  |
+   +---+
+    O   |
+  o-|-o  |
+   / \  | 
+   +---+
+    O   |
+  o-|-o |
+   / \  |
+ 
     ======''']
 
 # Pick Word from List 
@@ -50,6 +62,13 @@ def getRandomWord(wordList): # Return a random word from the list
     wordIndex = random.randint(0, len(wordList)-1) 
     # len(listname) -1 is extremely common for working with lists.
     return wordList[wordIndex] 
+
+# Pick Word from Dictionary
+def getRandomWord(wordDict): # Return a random word from the list
+    wordKey = random.choice(list(wordDict.keys()))
+    wordIndex = random.randit(o,len(wordDict[wordKey])-1) 
+    # len(listname) -1 is extremely common for working with lists.
+    return [wordDict[wordKey][wordIndex], wordKey]
 
 def displayBoard(missedLetters, correctLetters, sercetWord):
     print(HANGMAN_BOARD[len(missedLetters)])
@@ -92,11 +111,25 @@ def playAgain():
 
 # Introduce the Game 
 print('Welcome to Hangman by Marcus B.')
+
+# CHOOSE DIFFICULTY 
+difficulty = 'X'
+while difficulty not in 'EMH':
+    print('Please choose Easy, Medium or Hard. Type the first letter then press enter.\n')
+    difficulty = input().upper()
+if difficulty == 'M': # MEDIUM
+    del HANGMAN_BOARD[8]
+    del HANGMAN_BOARD[7]
+if difficulty == 'H': # HARD
+    del HANGMAN_BOARD[8]
+    del HANGMAN_BOARD[7]
+    del HANGMAN_BOARD[5]
+    del HANGMAN_BOARD[3] 
+
 missedLetters = ''
 correctLetters = ''
 secretWord = getRandomWord(words)
-gameIsDone = False
-
+gameIsDone = False 
 # Main Game Loop
 while True:
     displayBoard(missedLetters, correctLetters, secretWord) 
@@ -106,16 +139,17 @@ while True:
     if guess in secretWord:
         correctLetters = correctLetters + guess 
 
+        
         # Check to See if Winner, Winner Chicken Dinner
         foundAllLetters = True
         for i in range(len(secretWord)):
             if secretWord[i] not in correctLetters:
                 foundAllLetters = False 
                 break
-            if foundAllLetters:
-                print('Much wow! Very win! Well done.')
-                print('The secret word was' + secretWord)
-                gameIsDone = True
+        if foundAllLetters:
+            print('Much wow! Very win! Well done.')
+            print('The secret word was' + secretWord)
+            gameIsDone = True
     else:
         missedLetters = missedLetters + guess
 
