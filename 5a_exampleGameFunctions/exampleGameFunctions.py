6328bfbd4e3f5c1491d4039908c9e3130fe34541
MarcusJBrunson <s11758661@students.duvalschools.org>
2023-11-13 13:51:25 -0500

    Update exampleGameFunctions.py

--- 5a_exampleGameFunctions/exampleGameFunctions.py
+++ 5a_exampleGameFunctions/exampleGameFunctions.py
@@ -1,1 +1,14 @@
 # Example Game Functions Project, Marcus Brunson, v0.0
+import random
+
+def functionOne():
+    pass
+
+def functionTwo(param1):
+    pass
+
+def functionThree(param1 = "Default Value"):
+    print(param1)
+
+    def functionFour(param1, param2, param3):
+        pass
