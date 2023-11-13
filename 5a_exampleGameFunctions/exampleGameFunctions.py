cda092e39dd670300d78034c6461b6cb790764f6
MarcusJBrunson <s11758661@students.duvalschools.org>
2023-11-13 14:46:38 -0500

    Update exampleGameFunctions.py

--- 5a_exampleGameFunctions/exampleGameFunctions.py
+++ 5a_exampleGameFunctions/exampleGameFunctions.py
@@ -1,8 +1,16 @@
 # Example Game Functions Project, Marcus Brunson, v0.0
 import random
+# DECLARATIOINS
+playername = ""
+playerHealth = 100
+CPUHealth = 100
+difficulty = ""
+Damage = 
 
-def functionOne():
-    pass
+def functionOne(difficulty):
+    if difficulty == "Easy":
+        print("You have selected Easy as your difficulty")
+
 
 def functionTwo(param1):
     pass
@@ -12,3 +20,18 @@ def functionThree(param1 = "Default Value"):
 
     def functionFour(param1, param2, param3):
         pass
+
+    def catchBall(throwQuality, passCatcherScore, weather):
+        if throwQuality > 5.0 and passCatcherScore >= 99 and weather == 'Sunny':
+            ballcaught = True
+        elif throwQuality > 4.0 and passCatcherScore >= 75 and weather == 'Windy':
+            ballcaught = False
+        else:
+            print('oh, no, interception!\n')
+            ballIntercepted = True
+        return ballcaught
+    
+    catchBall(4.25, 107, 'Rainy')
+
+    
+        
