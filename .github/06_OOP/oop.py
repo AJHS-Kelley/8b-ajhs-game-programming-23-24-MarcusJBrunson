63f492dd726a476e2e06c1eb3e66d8eba60efce8
MarcusJBrunson <s11758661@students.duvalschools.org>
2023-12-12 14:11:10 -0500

    Update oop.py

--- .github/06_OOP/oop.py
+++ .github/06_OOP/oop.py
@@ -10,7 +10,28 @@ class Person: # Use PascalCase for ClassNames
     def __str__(self):
         return f"Name: {self.name}\nThis person is {self.age} years old.\nThey weigh {self.weight} pounds.\n"
 
-
+    def classfunction(self):
+        print("This is an example class function")
+        print("It only works when called on an object of that class.")
 
 person1 = Person("Eren Yeager", 19, 140)
+person2 = Person("Gojo", 22, 160)
 print(person1)
+print(person2)
+
+if person1.weight > person2.weight:
+    print("f{person1.name} weighs more than {person2.name}.")
+elif person1.weight == person2.weight:
+    print("Each person weighs the same.\n")
+else: 
+    print(f"{person2.name} weighs more than {person1.name}")
+
+if person1.age > person2.age:
+    print("f{person1.name} is older than {person2.name}.")
+elif person1.age == person2.age:
+    print("Each person is the same age.\n")
+else: 
+    print(f"{person2.name} is older than {person1.name}")
+
+person1.classfunction()
+
