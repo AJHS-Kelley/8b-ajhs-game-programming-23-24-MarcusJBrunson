237a9800b49b27b10cfa48812e1e1614f17481f9
MarcusJBrunson <s11758661@students.duvalschools.org>
2023-12-12 13:52:54 -0500

    Update oop.py

--- .github/06_OOP/oop.py
+++ .github/06_OOP/oop.py
@@ -1,3 +1,16 @@
 # Object-Oriented prgramming, Marcus Brunson, v0.0 
 
 class Person: # Use PascalCase for ClassNames
+    def __init__(self, name, age, weight):
+        self.name = name 
+        self.age = age 
+        self.weight = weight
+
+    # To string Function -- How the object appears as a string
+    def __str__(self):
+        return f"Name: {self.name}\nThis person is {self.age} years old.\nThey weigh {self.weight} pounds.\n"
+
+
+
+person1 = Person("Eren Yeager", 19, 140)
+print(person1)
