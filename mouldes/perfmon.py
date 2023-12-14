1c90527406f072ca55e6c34a7de14c97330156e3
MarcusJBrunson <s11758661@students.duvalschools.org>
2023-12-14 14:20:41 -0500

    exploding dice

---
+++ mouldes/perfmon.py
@@ -0,0 +1,10 @@
+# Python Performance Monitoring Module by Marcus Brunson, v0.0
+import time
+
+def execStart():
+    startTime = time.time()
+    return startTime
+
+def execStop(): 
+    stopTime = time.time()
+    return stopTime
--- mouldes/tester.py
+++ mouldes/tester.py
@@ -1,10 +1,15 @@
 import dice 
 
 
-dice.display(1,6)
-dice.display(1,6)
+roll1= dice.roll(1,6)
+roll2= dice.display(1,6)
 
 if dice.isDoubles(roll1, roll2):
     print("You rolled a double, go again!\n")
 else:
     print("It was not a double.\n")
+
+if dice.isExploding(roll1, 6):
+    print("This roll exploded!\n")
+    roll1 += dice.roll(1,6)
+    print(roll1)
