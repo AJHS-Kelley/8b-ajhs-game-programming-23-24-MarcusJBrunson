72951c1da26f4e06ab74547dd7007ee11b567691
MarcusJBrunson <s11758661@students.duvalschools.org>
2023-12-14 13:36:11 -0500

    Create dice.py

---
+++ mouldes/dice.py
@@ -0,0 +1,13 @@
+# Dice Rolling Module by Marcus Brunson, v0.0
+import random
+
+def rollDice(numDice, sizeDice):
+   numRolled = 0
+   sum = 0
+   while numRolled < numDice: 
+        roll = random.randint(1, sizeDice)
+        sum += roll
+        print(f"Roll: {roll}\n")
+        print(f"Sum: {sum}\n")
+        numRolled += 1
+        return sum
