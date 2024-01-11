1808b519ae73c2a46627674d05fcccaadff89bb6
MarcusJBrunson <s11758661@students.duvalschools.org>
2024-01-11 14:01:37 -0500

    Create dnaGame.py

---
+++ gameProgramming/cohort_lessons/dnaGame/dnaGame.py
@@ -0,0 +1,7 @@
+# Dna Replication Game, Marcus Brunson, v0.0
+
+# Import Entire Modules -- Get the whole tool box.
+import time, datetime 
+
+# Import Specific Methods -- Get the specific tool. 
+from random import choice
