4a58a66620ebb7deddf2a7ff536e9dd17f1ea148
MarcusJBrunson <s11758661@students.duvalschools.org>
2024-01-16 14:12:00 -0500

    Update dnaGame.py

--- gameProgramming/cohort_lessons/dnaGame/dnaGame.py
+++ gameProgramming/cohort_lessons/dnaGame/dnaGame.py
@@ -22,15 +22,14 @@ def genDNA() -> str:
         basesGenerated += 1
     return dnaSequence
 
+dna = genDNA()
 
-dna = genDNA
-
-def genRNA(dnaSequence: str) -> tuple:
+def doTranscription(dnaSequence: str) -> tuple:
     print(f"The DNA Sequence is {dnaSequence}.\n")
     print("You will now generate the RNA sequence that would match.\n")
     print("Please remember, in the RNA sequence U pairs with A from the sequence.\n")
     rnaStart= time.time() # time.time() returns the number of seconds since 00:00:00 UTC Jan, 01, 1970
-    rnaSequence = input("Please enter the matching RNA sequence. Leave no spaces! The press enter.\n").upper
+    rnaSequence = input("Please enter the matching RNA sequence. Leave no spaces! The press enter.\n").upper()
     rnaStop = time.time()
     rnaTime = rnaStop - rnaStart 
     return (rnaSequence, rnaTime)
@@ -38,8 +37,30 @@ def genRNA(dnaSequence: str) -> tuple:
     # Tuples are unchangeable -- you cannot add, modify, or delete after creating 
     # Tuples CAN have duplicate values.
 
-rna = genRNA(dna)
-print(rna)
+def verifySequence(dnaSequence: str, rnaSequence: str) -> bool:
+    isMatch = False
+    if len(dnaSequence) != len(rnaSequence):
+        print("The Sequences are different lengths and cannot match.\n")
+        return isMatch 
+    for dnaBase, rnaBase in zip(dnaSequence, rnaSequence):
+        if dnaBase == "A" and rnaBase == "U":
+            isMatch = True
+        elif dnaBase == "C" and rnaBase == "G":
+            isMatch = True
+        elif dnaBase == "G" and rnaBase == "C":
+            isMatch = True
+        elif dnaBase == "T" and rnaBase == "A":
+            isMatch = True
+        else:
+                print("Unable to identify correct base so no match")
+                return isMatch 
+
+        
+
+
+dna = genDNA()
+rna = doTranscription(dna)
+print(verifySequence(dna, rna[0]))
 
 
 
