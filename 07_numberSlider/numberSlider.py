7018815bc2251d4c3f8f54f8e34ce5350bbdfbec
MarcusJBrunson <s11758661@students.duvalschools.org>
2024-01-22 13:58:13 -0500

    Update numberSlider.py

--- 07_numberSlider/numberSlider.py
+++ 07_numberSlider/numberSlider.py
@@ -1,4 +1,4 @@
-# Number Slider, by Marcus Brunson, based on project by Al Sweigart, v0.0
+# Number Slider, by Marcus Brunson, based on project by Al Sweigart, v0.1
 
 import sys, random, pygame
 # sys module provides access to system resources (i,e. Operating System Commands)
@@ -6,4 +6,12 @@ import sys, random, pygame
 from pygame.locals import *
 # Allows us to call functions from pygame using just the function name instead of module.function()
 # Example: We can use draw() instead of pygame.draw()
- 
+
+# Constants for Game Broad 
+BOARDWIDTH = 4 # Columns 
+BOARDHEIGHT = 4 # ROWS
+TITLESIZE = 80 # Measured in Pixels 
+WINDOWWIDTH = 640
+WINDOWHEIGHT = 480
+FPS = 30
+BLANK = None
