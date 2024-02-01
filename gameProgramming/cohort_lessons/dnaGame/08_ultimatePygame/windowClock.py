6f3adc3d9b6572da61803b6269fb26f5f8dbd1bc
MarcusJBrunson <s11758661@students.duvalschools.org>
2024-02-01 14:09:01 -0500

    updating font and text surface

--- gameProgramming/cohort_lessons/dnaGame/08_ultimatePygame/windowClock.py
+++ gameProgramming/cohort_lessons/dnaGame/08_ultimatePygame/windowClock.py
@@ -5,8 +5,11 @@ pygame.init()
 screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption('Runner')
 Clock = pygame.time.Clock()
-sky_surface = pygame.image.load('graphics/Sky.png')
-ground_surface = pygame.image.load('graphics/ground.png')
+test_font = pygame.font.Font(None, 50)
+
+sky_surface = pygame.image.load('img/ultimatePygame/Sky.png')
+ground_surface = pygame.image.load('img/ultimatePygame/ground.png')
+text_surface = test_font.render('My game', False, 'Green')
 
 while True:
     for event in pygame.event.get():
@@ -16,6 +19,7 @@ while True:
 
     screen.blit(sky_surface,(0,0))
     screen.blit(ground_surface,(0,300))
+    screen.blit(text_surface, (300,50))
 
     pygame.display.update()
     Clock.tick(60)
