e91baadc5380c95863cd959d18a9a48f243a5be1
MarcusJBrunson <s11758661@students.duvalschools.org>
2024-02-13 14:18:51 -0500

    Update windowClock.py

--- gameProgramming/cohort_lessons/dnaGame/08_ultimatePygame/windowClock.py
+++ gameProgramming/cohort_lessons/dnaGame/08_ultimatePygame/windowClock.py
@@ -7,12 +7,14 @@ pygame.display.set_caption('Runner')
 Clock = pygame.time.Clock()
 test_font = pygame.font.Font(None, 50)
 
-sky_surface = pygame.image.load('img/ultimatePygame/Sky.png')
-ground_surface = pygame.image.load('img/ultimatePygame/ground.png')
+sky_surface = pygame.image.load('img/ultimatePygame/Sky.png').convert()
+ground_surface = pygame.image.load('img/ultimatePygame/ground.png').convert()
 text_surface = test_font.render('My game', False, 'Green')
 
-snail_surface = pygame.image.load('img/ultimatePygame/snail1.png')
+snail_surface = pygame.image.load('img/ultimatePygame/snail1.png').convert_alpha()
 snail_x_pos = 600
+
+player_surface = pygame.image.load('img/ultimatePygame/player_walk_1.png').convert_alpha
 
 while True:
     for event in pygame.event.get():
@@ -24,6 +26,7 @@ while True:
     screen.blit(ground_surface,(0,300))
     screen.blit(text_surface,(300,50))
     snail_x_pos -= 4
+    if snail_x_pos < 100: snail_x_pos = 800
     screen.blit(snail_surface,(snail_x_pos,250))
 
     pygame.display.update()
