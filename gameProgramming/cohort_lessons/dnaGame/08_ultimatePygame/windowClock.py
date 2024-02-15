36b84208c2562e038b74465c34b2d9da68fb4677
MarcusJBrunson <s11758661@students.duvalschools.org>
2024-02-15 09:32:52 -0500

    Update windowClock.py

--- gameProgramming/cohort_lessons/dnaGame/08_ultimatePygame/windowClock.py
+++ gameProgramming/cohort_lessons/dnaGame/08_ultimatePygame/windowClock.py
@@ -22,6 +22,8 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+        # if event.type == pygame.MOUSEMOTION:
+        #     if player_rect.collidepoint(event.pos): print('collision')
 
     screen.blit(sky_surface,(0,0))
     screen.blit(ground_surface,(0,300))
@@ -34,9 +36,10 @@ while True:
 
     # if player_rect.colliderect(snail_rect):
     #     print('collision')
-    mouse_pos = pygame.mouse.get_pos()
-    if player_rect.collidepoint(mouse_pos):
-        print('collision')
+
+    # mouse_pos = pygame.mouse.get_pos()
+    # if player_rect.collidepoint(mouse_pos):
+    #     print(pygame.mouse.get_pressed())
 
     
     pygame.display.update()
