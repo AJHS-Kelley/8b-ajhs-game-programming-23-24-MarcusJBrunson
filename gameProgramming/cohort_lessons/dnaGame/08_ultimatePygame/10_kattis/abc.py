ab97c708fa1bd2b096d504269a35e0c5b0e71f23
MarcusJBrunson <s11758661@students.duvalschools.org>
2024-04-01 14:01:17 -0400

    Create abc.py

---
+++ gameProgramming/cohort_lessons/dnaGame/08_ultimatePygame/10_kattis/abc.py
@@ -0,0 +1,38 @@
+# input the intergers 
+# split() the 3 intergers into separate variables 
+# cast the 3 variables to intergers 
+# assign correct values from least to greatest 
+# A < B < C 
+
+intergers = intput()
+a, b, c, = intergers.split()
+a = int(a)
+b = int(b)
+c = int(c) 
+
+print(f"a: {a} b: {b} c: {c}")
+if a >= b:
+    a, b = b, a 
+if b >= c:
+    b, c = c, b 
+    b <= a: 
+    b, a = a, b
+print(f"a: {a} b: {b} c: {c}") 
+
+# input the string variable 
+#determine order of A, B, and C 
+# create correct string 
+# output string 
+
+order = input()
+myString = "" 
+
+for i in range(len(order)): 
+    if order [i] == "A":
+        myString += str(a) + "  "
+    elif CODEGOESHERE:
+        CODEGOESHERE
+    else: 
+        CODEGOESHERE
+
+    print(myString) 
