0a732be4d57777e44e476617eb5e5a62ac16e45f
MarcusJBrunson <s11758661@students.duvalschools.org>
2024-04-11 14:13:02 -0400

    Update finalProject.py

--- .github/09_finalProject.py/finalProject.py
+++ .github/09_finalProject.py/finalProject.py
@@ -1,5 +1,6 @@
 # Final Project, Marcus Brunson, v0.0
-import random, pygame 
+import random, pygame
+from sys import exit  
 
 resolution = 0 # 0 = Low Resolution (800, 600), 1 = High Resolution (1920, 1080)
 
@@ -10,26 +11,26 @@ else:
     x = 1920 
     y = 1080
 
-pygame.init()
 
 difficulty = int(input("Please choose a difficulty. Enter 1 for Easy or 2 for HARD.\n"))
 
 if difficulty == 1:
     pygame.display.set_caption('NAME OF GAME -- EASY')
 else: 
-    pygame.display.set_caption('NAME OF GAME -- HARD')
+    pygame.display.set_caption('NAME OF GAME -- HARD') 
 
-pygame.display.set_caption('MARCUS Golden Axe -- EASY')
-pygame.display.set_caption('MARCUS Golden Axe -- HARD')
+pygame.init() 
 screen = pygame.display.set_mode((x, y))  
-# CREATE AN IF / ELSE BLOCK TO SET THE RESOLUTION BASED ON THE VARIABLE ABOVE.
+pygame.display.set_caption('MARCUS Golden Axe -- EASY')
+pygame.display.set_caption('MARCUS Golden Axe -- HARD') 
+clock = pygame.time.Clock() 
 
 # Colors 
-White = (255, 255, 255)
-Black = (0, 0, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
 
-player_image = pygame.image.load().convert()
-enemy_image = pygame.image.load().convert()
-background_image = pygame.image.load().convert()
+# player_image = pygame.image.load().convert()
+# enemy_image = pygame.image.load().convert()
+# background_image = pygame.image.load('img/ultimatePygame/Sky.png').convert()
 
 
